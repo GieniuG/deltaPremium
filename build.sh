3a8c19ebac586860ee093b363ec0f1e99e485f1a
GieniuG <gieniu@gieniuArch.localdomain>
2025-12-29 01:39:36 +0100

    refine build process

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 echo "> Build for chrome"
+cp manifest.json.chrome manifest.json
 npx web-ext build --ignore-files="assets/**" --overwrite-dest --filename="deltaPremium.zip"
 
 echo ""
 echo "> Build for firefox"
-npx web-ext sign --ignore-files="assets/**" --api-key=$AMO_KEY --api-secret=$AMO_SECRET --channel="unlisted"
+cp manifest.json.firefox manifest.json
+if [[ "$1" == "local" ]];then
+    npx web-ext build --ignore-files="assets/**" --overwrite-dest --filename="deltaPremium.xpi"
+else
+    npx web-ext sign --ignore-files="assets/**" --api-key=$AMO_KEY --api-secret=$AMO_SECRET --channel="unlisted"
+fi
+cp manifest.json.chrome manifest.json
