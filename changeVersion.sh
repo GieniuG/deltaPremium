62a0edefe653aa3b0fdc622bfe805e550306f855
GieniuG <bartoszlwowski@gmail.com>
2026-05-20 17:07:50 +0200

    add a script to easily change version in manifests

---
+++ changeVersion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "current version `cat manifest.json | jq .version`"
+
+read new_version
+regex='s/"version":.*/"version":"'$new_version'"/g'
+
+
+sed -i $regex manifest.json.chrome 
+sed -i $regex manifest.json.firefox 
+
