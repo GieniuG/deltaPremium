ab6684317d2667579d55fcdaf058dc3a52dcc042
GieniuG <bartoszlwowski@gmail.com>
2026-05-20 17:10:44 +0200

    add a missing comma during substitution

--- changeVersion.sh
+++ changeVersion.sh
@@ -4,7 +4,7 @@
 echo "current version `cat manifest.json | jq .version`"
 
 read new_version
-regex='s/"version":.*/"version":"'$new_version'"/g'
+regex='s/"version":.*/"version":"'$new_version'",/g'
 
 
 sed -i $regex manifest.json.chrome 
