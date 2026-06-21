636c7279798cbc0e1fa116bde2b5c1f509b1a6dc
GieniuG <bartoszlwowski@gmail.com>
2026-06-22 01:11:49 +0200

    make build script more universal

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 echo "> Build for chrome"
-name="deltapremium-$(jq -r ".version" manifest.json.chrome).zip"
 cp manifest.json.chrome manifest.json
-npx web-ext build --ignore-files="assets/**" --overwrite-dest --filename="$name"
+name="$(jq -r ".name" manifest.json)-$(jq -r ".version" manifest.json)"
+npx web-ext build --ignore-files="assets/**" --overwrite-dest --filename="$name.zip"
 
 echo ""
 echo "> Build for firefox"
 cp manifest.json.firefox manifest.json
 if [[ "$1" == "local" ]];then
-    npx web-ext build --ignore-files="assets/**" --overwrite-dest --filename="deltaPremium.xpi"
+    npx web-ext build --ignore-files="assets/**" --overwrite-dest --filename="$name.local.xpi"
 else
     npx web-ext sign --ignore-files="assets/**" --api-key=$AMO_KEY --api-secret=$AMO_SECRET --channel="unlisted"
 fi
 cp manifest.json.chrome manifest.json
+
